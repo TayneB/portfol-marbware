4bc0b4a752dd7281f7e43f24df92703f184a4a7c
Tayne Bilyard <taynebilyard@gmail.com>
2024-07-03 22:40:57 +1200

    css added to nav

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,27 +1,96 @@
 'use client'
-import { usePathname } from 'next/navigation'
+
 import Link from 'next/link'
+import { useState } from 'react'
 
 export default function Nav() {
-  const pathname = usePathname()
-
+  let [selectedHome, setSelectedHome] = useState('true')
+  let [selectedProj1, setSelectedProj1] = useState('right')
+  let [selectedProj2, setSelectedProj2] = useState('right')
+  let [selectedProj3, setSelectedProj3] = useState('right')
+  let [selectedAbout, setSelectedAbout] = useState('right')
+  function handleClickHome() {
+    setSelectedHome('true')
+    setSelectedProj1('right')
+    setSelectedProj2('right')
+    setSelectedProj3('right')
+    setSelectedAbout('right')
+  }
+  function handleClick1() {
+    setSelectedHome('left')
+    setSelectedProj1('true')
+    setSelectedProj2('right')
+    setSelectedProj3('right')
+    setSelectedAbout('right')
+  }
+  function handleClick2() {
+    setSelectedHome('left')
+    setSelectedProj1('left')
+    setSelectedProj2('true')
+    setSelectedProj3('right')
+    setSelectedAbout('right')
+  }
+  function handleClick3() {
+    setSelectedHome('left')
+    setSelectedProj1('left')
+    setSelectedProj2('left')
+    setSelectedProj3('true')
+    setSelectedAbout('right')
+  }
+  function handleClickAbout() {
+    setSelectedHome('left')
+    setSelectedProj1('left')
+    setSelectedProj2('left')
+    setSelectedProj3('left')
+    setSelectedAbout('true')
+  }
   return (
-    <nav>
+    <nav className={`nav-bar`}>
       <ul>
         <li>
-          <Link href="/">home</Link>
+          <Link
+            href="/"
+            className={`${selectedHome}`}
+            onClick={handleClickHome}
+          >
+            home
+          </Link>
         </li>
         <li>
-          <Link href="/project1">project 1</Link>
+          <Link
+            href="/project1"
+            className={`${selectedProj1}`}
+            onClick={handleClick1}
+          >
+            project 1
+          </Link>
         </li>
         <li>
-          <Link href="/project2">project 2</Link>
+          <Link
+            href="/project2"
+            className={`${selectedProj2}`}
+            onClick={handleClick2}
+          >
+            project 2
+          </Link>
         </li>
         <li>
-          <Link href="/project3">project 3</Link>
+          <Link
+            href="/project3"
+            className={`${selectedProj3}`}
+            onClick={handleClick3}
+          >
+            project 3
+          </Link>
         </li>
         <li>
-          <Link href="/about">about</Link>
+          <Link
+            href="/about"
+            className={`${selectedAbout}`}
+            onClick={handleClickAbout}
+          >
+            about
+          </Link>
         </li>
       </ul>
     </nav>
