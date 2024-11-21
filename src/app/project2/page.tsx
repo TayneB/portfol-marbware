0518e1401c849a51b5820a459a8cd29ed1170ac7
Tayne Bilyard <taynebilyard@gmail.com>
2024-11-21 19:17:47 +1300

    Organised time parses

--- src/app/project2/page.tsx
+++ src/app/project2/page.tsx
@@ -1,19 +1,31 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Proj2Page: React.FC = () => {
   return (
     <div className="page-content">
-      <h1>Time Parses</h1>
-      <p>
-        The time parses app is fullstack built with javascript utilising react
-        and typescript. It integrates with the third party GraphQL Warcraftlogs
-        Public Api that players use to record their performance during
-        encounters in the massively multiplayer online roleplaying game Game
-        World of Warcraft. By inputing a characters name, server, region and
-        specified encounter, the app allows user to search for the best parses
-        to compare against their own with a focus on cotrolling the data for
-        encounter duration.
-      </p>
+      <span className="page-content-grid">
+        <h1 className="timeParses-h1">
+          Time <br /> Parses
+        </h1>
+        <Image
+          className="timeParses-image"
+          alt="time parses website layout"
+          src="/TimeParses.png"
+          width={580 / 2}
+          height={571 / 2}
+        />
+        <p className="timeParses-p">
+          The time parses app is fullstack built with javascript utilising react
+          and typescript. It integrates with the third party GraphQL
+          Warcraftlogs Public Api that players use to record their performance
+          during encounters in the massively multiplayer online roleplaying game
+          Game World of Warcraft. By inputing a characters name, server, region
+          and specified encounter, the app allows user to search for the best
+          parses to compare against their own with a focus on cotrolling the
+          data for encounter duration.
+        </p>
+      </span>
     </div>
   )
 }
