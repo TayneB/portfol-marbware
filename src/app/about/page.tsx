1910919b36c19347f936158924c189d63e27012a
Tayne Bilyard <taynebilyard@gmail.com>
2024-11-29 18:12:22 +1300

    fixed about page css layout

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 const AboutPage: React.FC = () => {
   return (
     <div className="page-content">
-      <h1>About</h1>
-      <p>This portfolio is built in nextjs with typescript.</p>
-      <p>Hosted with the power of Amplify!</p>
+      <h1 className="about-h1">About</h1>
+      <p className="about-p">
+        This portfolio is built in nextjs with typescript.
+      </p>
+      <p className="about-p">Hosted with the power of Amplify!</p>
     </div>
   )
 }
